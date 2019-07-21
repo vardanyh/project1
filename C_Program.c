a4b10f6d5732bdd1cb32018b31c7802b80f4468d
vardanyh <hovhannes@gmail.com>
2019-07-21 23:31:26 +0000

    Add files via upload

---
+++ C_Program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+printf(“Hello! I am C-Program. Now modified. Thank you!”);
+
+return 0;
+
+}
+
+
+
+
+
---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+
+public static void main(String args[]){
+
+System.out.Println(“ Hello World! ”);
+
+}
+
+}
+
+
+
+
